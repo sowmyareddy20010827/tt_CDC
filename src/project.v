0627f4154adcfa58299ba87fdaad7bcc8292e4f8
sowmya <pothulasowmya408@gmail.com>
2025-08-30 22:35:52 -0700

    made changes for the module name2

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_umcdc_fifo (
+module tt_um_cdc_fifo (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path

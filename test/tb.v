d189eab2cbe758e5ae673da3aeb75279c2a5d84c
sowmya <pothulasowmya408@gmail.com>
2025-08-31 19:31:37 -0700

    changes made in project,tb,info files

--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_cdc_fifo user_project (
+  tt_um_anudeeshcdc_fifo user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST

954f08a156cddf0082a5002e56bb7cc9a0f488fe
sowmya <pothulasowmya408@gmail.com>
2025-08-31 20:51:42 -0700

    7

--- src/cdc_fifo_write_state.sv
+++ src/cdc_fifo_write_state.sv
@@ -41,28 +41,4 @@ module cdc_fifo_write_state #(
 
 endmodule
 
-module binary_to_gray #(
-  parameter WIDTH = 8
-) (
-  input logic [WIDTH-1:0] binary,
-  output logic [WIDTH-1:0] gray
-);
 
-  assign gray = (binary >> 1) ^ binary;
-
-endmodule
-
-module gray_to_binary #(
-  parameter WIDTH = 8
-) (
-  input logic [WIDTH-1:0] gray,
-  output logic [WIDTH-1:0] binary
-);
-
-  always_comb begin
-    for (int i = 0; i < WIDTH; i++) begin
-      binary[i] = ^(gray >> i);
-    end
-  end
-
-endmodule
